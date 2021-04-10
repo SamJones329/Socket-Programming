a499772b15a01685d61ca2c99a35675024b25c70
George Adler Buras <gburas4@lsu.edu>
2021-04-10 14:37:55 -0500

    Created shell scripts for client and server

---
+++ RunClient.sh
@@ -0,0 +1,7 @@
+# This shell script compiles and runs client
+
+# @author George Adler Buras
+# @since 4/10/2021
+
+gcc client.c -o client
+./client
---
+++ RunServer.sh
@@ -0,0 +1,7 @@
+# This shell script compiles and runs server
+
+# @author George Adler Buras
+# @since 4/10/2021
+
+gcc server.c -o server
+./server
