fd1d756ca4e81aeecd3e7be2fc2d3f2484beca66
akardorff <60453339+akardorff@users.noreply.github.com>
2021-04-14 10:45:39 -0500

    Update client.c

--- client.c
+++ client.c
@@ -9,41 +9,53 @@
    
 int main(int argc, char const *argv[])
 {
-    int sock = 0, valread;
+    int sock = 0;
+    int valread;
     struct sockaddr_in serv_addr;
-   
-   //allow user to send any message within the CHAR_LIMIT
     char *msg;
     msg = (char*) calloc(CHAR_LIMIT, sizeof(char));
-    printf("insert message to send to server: ");
-    fgets(msg, (50*sizeof(char)), stdin);
+    char fromServer[1024] = {0};    
+	
+    //will loop to allow the client to send as many messages as desired
+    while(1){
     
-    
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
+    	    //ask for user input to determine message to send
+	    printf("insert message to send to server, or type '/exit' to end program: \n");
+	    fgets(msg, (50*sizeof(char)), stdin);
+	    
+	    if(strncmp(msg, "/exit", 5) == 0){
+	    	free(msg);
+	    	return 0; 
+	    }
+	    
+
+	    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	    {
+		printf("\n Socket creation error \n");
+		return -1;
+	    }
+	   
+	    serv_addr.sin_family = AF_INET;
+	    serv_addr.sin_port = htons(PORT);
+	       
+	    // Convert IPv4 and IPv6 addresses from text to binary form
+	    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	    {
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	    }
+	   
+	    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	    {
+		printf("\nConnection Failed \n");
+	    }
+	    else{
+	    	send(sock , msg , (CHAR_LIMIT*sizeof(char)) , 0 );
+		printf("message sent: %s\n", msg);
+		valread = read( sock , fromServer, 1024);
+		printf("%s\n",fromServer );
+	    }
     }
-   
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-       
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-   
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    send(sock , msg , (CHAR_LIMIT*sizeof(char)) , 0 );
-    printf("message sent: %s\n", msg);
-    valread = read( sock , buffer, 1024);
-    printf("%s\n",buffer );
-    return 0;
+
+
 }
