ab8cb039d11568fca0671f5ad8fa9768e2902697
George Adler Buras <gburas4@lsu.edu>
2021-04-16 02:46:44 -0500

    Fixed bloat, making unlimited messages functional at speed

--- client.c
+++ client.c
@@ -16,33 +16,33 @@ int main(int argc, char const *argv[])
     msg = (char*) calloc(CHAR_LIMIT, sizeof(char));
     char fromServer[1024] = {0};    
 	
-    //will loop to allow the client to send as many messages as desired
-    while(1)
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-	    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	    {
-			printf("\n Socket creation error \n");
-			return -1;
-	    }
-	   
-	    serv_addr.sin_family = AF_INET;
-	    serv_addr.sin_port = htons(PORT);
-	       
-	    // Convert IPv4 and IPv6 addresses from text to binary form
-	    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
-	    {
-			printf("\nInvalid address/ Address not supported \n");
-			return -1;
-	    }
-	   
-	    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	    {
-			printf("\nConnection Failed \n");
-			return -1;
-	    }
-	    else
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+	
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+		
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+	
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	else
+	{
+		//will loop to allow the client to send as many messages as desired
+		while(1)
 		{
-	    	//ask for user input to determine message to send
+			//ask for user input to determine message to send
 			printf("insert message to send to server, or type '/exit' to end program (limit %d characters): \n", CHAR_LIMIT-1);
 			fgets(msg, (50*sizeof(char)), stdin);
 			
@@ -56,8 +56,6 @@ int main(int argc, char const *argv[])
 			printf("message sent: %s\n", msg);
 			valread = read( sock , fromServer, 1024);
 			printf("%s\n",fromServer );
-	    }
-    }
-
-
+		}
+	}
 }
